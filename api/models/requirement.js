a19efa4c59de0dc570690a7191ebde29da7df438
Ritesh Sangwan <sangwan.ritesh@yahoo.in>
2014-10-24 00:21:20 +0530

    db migration changes

---
+++ api/models/requirement.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2014 TopCoder, Inc. All rights reserved.
+ */
+
+/**
+ * Represent Requirement in the system.
+ *
+ * @version 1.0
+ * @author spanhawk
+ */
+'use strict';
+
+/**
+* Defining Requirement model
+*/
+module.exports = function(sequelize, DataTypes) {
+
+  var Requirement = sequelize.define('Requirement', {
+    // primary key
+    id: {
+      type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true,
+      get: function() {
+        return parseInt(this.getDataValue('id'));
+      }
+    },
+    challengeId: {
+      type: DataTypes.BIGINT, allowNull: false,
+      get: function() {
+        return parseInt(this.getDataValue('challengeId'));
+      }
+    },
+    requirementText : DataTypes.TEXT
+  }, {
+    tableName : 'requirements',
+    associate : function(models) {
+      Requirement.belongsTo(models.Challenge, {foreignKey : 'challengeId'});
+    }
+  });
+
+  return Requirement;
+
+};
---
+++ config/schema-migrations/20141023181555-requirements.js
@@ -0,0 +1,20 @@
+var dbm = require('db-migrate');
+var async = require('async');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+    // requirements table
+    db.runSql.bind(db,
+      'CREATE TABLE requirements ( ' +
+        'id bigserial NOT NULL, ' +
+        '"requirementText" text, ' +
+        '"challengeId" bigint NOT NULL REFERENCES challenges("id") ON UPDATE CASCADE ON DELETE SET NULL ' +
+      ');')
+    ], callback);
+};
+
+exports.down = function(db, callback) {
+    async.series([
+        db.dropTable.bind(db, 'requirements')
+    ], callback);
+};
