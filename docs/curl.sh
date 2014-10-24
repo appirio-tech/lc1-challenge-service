33b049e1cb2aadb309be2f34047d0cf39851e5d8
Kyle Bowerman <kyle.bowerman@appirio.com>
2014-10-24 18:00:58 -0500

    update curl script

--- docs/curl.sh
+++ docs/curl.sh
@@ -1,11 +1,21 @@
 HOST="http://localhost:10010"
+#OST="http://lc1-challenge-service.herokuapp.com"
 
 
-# 1. get all challenges
+#clear the screen
+clear
 
-#curl http://localhost:10010/challenges
-#echo '\n'
 
+# SECTION 0. set up what to do
+MYPATH="/challenges"
+URL=$HOST$MYPATH
+INIT=false
+CREATE_CHALLENGES=false
+ADD_PARTICPANTS=false
+
+if  [ $INIT = "false" ] &&  [ $CREATE_CHALLENGES = "false" ] && [ $ADD_PARTICPANTS = "false" ]; then
+  echo 'You must set one var to be true, see Section 0 in the script  \n '
+fi
 
 # init section
 if [ $INIT = "true" ]; then
@@ -17,82 +27,108 @@ if [ $INIT = "true" ]; then
 fi
 
 
-
-# 1.  create  challenges
-MYPATH="/challenges"
-URL=$HOST$MYPATH
-INIT=false
+# Section 1 Create Challenges
+if [ $CREATE_CHALLENGES = "true" ]; then
 
 
-# 1.0
-# curl -H "Content-Type: application/json" -d '{"title": "minimal", "status": "DRAFT" }' $URL
+  # 1.1 Create a full challenge Draft challenge  (A1)
+  echo 'creating four challenges'
+
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "regStartAt": "2014-10-1",
+    "subEndAt": "2014-10-7",
+    "title": "One challenge to rule them all",
+    "overview": "This is the first challenge, it is a draft",
+    "description": "We are looking for a little more than just adding a pg module, this is a draft",
+    "tags": ["mean", "postgresql", "jsonb"],
+    "status": "DRAFT",
+    "account": "Singlilo",
+    "accountId": "100",
+    "prizes": [1600.00, 1000.00, 500.50],
+    "source": "lc"
+  }' $URL
+
+  # 1.2 Create a full challenge Asctive challenge  (A2)
+
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+    "regStartAt": "2014-10-2",
+    "subEndAt": "2014-10-22",
+    "title": "Two to Tango",
+    "overview": "teach us to dance",
+    "description": "write a document to teach us to dance",
+    "tags": ["postgresql", "jsonb"],
+    "status": "SUBMISSION",
+    "account": "Twolio in",
+    "accountId": "200",
+    "prizes": [1002, 502],
+    "source": "lc"
+  }' $URL
+
+  # 1.3 Create a full challenge Review challenge  (A3)
+
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "regStartAt": "2014-08-29",
+    "subEndAt": "2014-09-13",
+    "title": "Three is a magic number",
+    "overview": "teach us to magic",
+    "description": "Write a doc about magic",
+    "tags": ["postgresql"],
+    "status": "REVIEW",
+    "account": "Three Guys Co",
+    "accountId": "300",
+    "prizes": [3000],
+    "source": "tc",
+    "sourceId": "123xx456x789"
+  }' $URL
+
+  #1.4 Create a full challenge complete challenge  (A3)
+
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+    "regStartAt": "2014-07-1",
+    "subEndAt": "2014-07-17",
+    "title": "Four on the Floor",
+    "overview": "learn to drive a stick",
+    "description": "Create an architecture to describe how a manual transmision automobile works",
+    "tags": ["postgresql"],
+    "status": "COMPLETE",
+    "account": "Fourpols",
+    "accountId": "400",
+    "prizes": [4000, 400, 40, 4, 1],
+    "source": "tc",
+    "sourceId": "123xx456x789"
+  }' $URL
 
 
-# 1.1 Create a full challenge Draft challenge  (A1)
+  psql -c 'select id, title from challenges;'  -d travis_ci_test  -U postgres
 
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
-  "regStartAt": "2014-10-1",
-  "subEndAt": "2014-10-7",
-  "title": "One challenge to rule them all",
-  "overview": "This is the first challenge, it is a draft",
-  "description": "We are looking for a little more than just adding a pg module, this is a draft",
-  "tags": ["mean", "postgresql", "jsonb"],
-  "status": "DRAFT",
-  "account": "Singlilo",
-  "accountId": "100",
-  "prizes": [1600.00, 1000.00, 500.50],
-  "source": "lc"
-}' $URL
+fi
 
-# 1.2 Create a full challenge Asctive challenge  (A2)
+#add participants:
+if [ $ADD_PARTICPANTS = "true" ]; then
+  echo 'Adding Particpants'
 
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
-  "regStartAt": "2014-10-2",
-  "subEndAt": "2014-10-22",
-  "title": "Two to Tango",
-  "overview": "teach us to dance",
-  "description": "write a document to teach us to dance",
-  "tags": ["postgresql", "jsonb"],
-  "status": "SUBMISSION",
-  "account": "Twolio in",
-  "accountId": "200",
-  "prizes": [1002, 502],
-  "source": "lc"
-}' $URL
+  echo 'Adding owners to all four'
 
-# 1.3 Create a full challenge Review challenge  (A3)
+  RESOURCE="/1/participants"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "owner", "userId": 1 }' $URL$RESOURCE
 
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
-  "regStartAt": "2014-08-29",
-  "subEndAt": "2014-09-13",
-  "title": "Three is a magic number",
-  "overview": "teach us to magic",
-  "description": "Write a doc about magic",
-  "tags": ["postgresql"],
-  "status": "REVIEW",
-  "account": "Three Guys Co",
-  "accountId": "300",
-  "prizes": [3000],
-  "source": "tc",
-  "sourceId": "123xx456x789"
-}' $URL
+  echo 'Adding reviews, submitters and watchers to 2,3,4'
 
-#1.4 Create a full challenge complete challenge  (A3)
+  RESOURCE="/2/participants"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "owner", "userId": 1 }' $URL$RESOURCE
 
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
-  "regStartAt": "2014-07-1",
-  "subEndAt": "2014-07-17",
-  "title": "Four on the Floor",
-  "overview": "learn to drive a stick",
-  "description": "Create an architecture to describe how a manual transmision automobile works",
-  "tags": ["postgresql"],
-  "status": "COMPLETE",
-  "account": "Fourpols",
-  "accountId": "400",
-  "prizes": [4000, 400, 40, 4, 1],
-  "source": "tc",
-  "sourceId": "123xx456x789"
-}' $URL
+  RESOURCE="/3/participants"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "owner", "userId": 1 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "reviewer", "userId": 20 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "submitter", "userId": 30 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "watcher", "userId": 40 }' $URL$RESOURCE
 
+  RESOURCE="/4/participants"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "owner", "userId": 1 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "reviewer", "userId": 20 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "submitter", "userId": 30 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "watcher", "userId": 40 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "submitter", "userId": 50 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "watcher", "userId": 60 }' $URL$RESOURCE
 
-psql -c 'select id, title from challenges;'  -d travis_ci_test  -U postgres
+fi
