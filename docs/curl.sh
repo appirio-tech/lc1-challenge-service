886a8054a526d84f0bf02515f6276dc889dac34c
Kyle Bowerman <kyle.bowerman@appirio.com>
2014-10-24 23:30:57 -0500

    update curl script and postman file

--- docs/curl.sh
+++ docs/curl.sh
@@ -1,5 +1,5 @@
 HOST="http://localhost:10010"
-#OST="http://lc1-challenge-service.herokuapp.com"
+#HOST="http://lc1-challenge-service.herokuapp.com"
 
 
 #clear the screen
@@ -12,12 +12,18 @@ URL=$HOST$MYPATH
 INIT=false
 CREATE_CHALLENGES=false
 ADD_PARTICPANTS=false
+ADD_CHALLENGE_FILE=false
+ADD_SUBMISSION=false
+ADD_SUBMISSION_FILES=false
+ADD_REQUIREMENTS=false
 
-if  [ $INIT = "false" ] &&  [ $CREATE_CHALLENGES = "false" ] && [ $ADD_PARTICPANTS = "false" ]; then
+if  [ $INIT = "false" ] &&  [ $CREATE_CHALLENGES = "false" ] && [ $ADD_PARTICPANTS = "false" ] \
+  && [ $ADD_CHALLENGE_FILE = "false" ] && [ $ADD_SUBMISSION = "false" ] && [ $ADD_SUBMISSION_FILES = "false" ] \
+  &&  [ $ADD_REQUIREMENTS = "false" ]; then
   echo 'You must set one var to be true, see Section 0 in the script  \n '
 fi
 
-# init section
+# SECTION 1 DB init
 if [ $INIT = "true" ]; then
  psql -c 'DROP DATABASE IF EXISTS travis_ci_test;' -U postgres
   psql -c 'create database  travis_ci_test;' -U postgres
@@ -27,7 +33,7 @@ if [ $INIT = "true" ]; then
 fi
 
 
-# Section 1 Create Challenges
+# Section 2 Create Challenges
 if [ $CREATE_CHALLENGES = "true" ]; then
 
 
@@ -103,7 +109,7 @@ if [ $CREATE_CHALLENGES = "true" ]; then
 
 fi
 
-#add participants:
+# Section 3: add participants:
 if [ $ADD_PARTICPANTS = "true" ]; then
   echo 'Adding Particpants'
 
@@ -130,5 +136,204 @@ if [ $ADD_PARTICPANTS = "true" ]; then
   curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "watcher", "userId": 40 }' $URL$RESOURCE
   curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "submitter", "userId": 50 }' $URL$RESOURCE
   curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "role": "watcher", "userId": 60 }' $URL$RESOURCE
+
+fi
+
+
+# Section 4 Add cHallenge files (not submission Files)
+if [ $ADD_CHALLENGE_FILE = "true" ]; then
+  echo 'Adding challenge Files'
+
+  RESOURCE="/1/files"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+  "title": "The Highlander Challenge Guidelines",
+  "filePath": "/uploads/challengeId/",
+  "size": 123,
+  "fileName": "swords_forTheGatering.zip",
+  "storageLocation": "local"
+  }' $URL$RESOURCE
+
+  RESOURCE="/2/files"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+  "title": "Tango: Distributed Data Structures over a Shared Log",
+  "filePath": "www.cs.cornell.edu/~taozou/sosp13/tangososp.pdf",
+  "size": 123,
+  "fileName": "tangososp.pdf",
+  "storageLocation": "local"
+  }' $URL$RESOURCE
+
+  RESOURCE="/3/files"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+  "title": "Magic Tricks for the Beginning Magician",
+  "filePath": "http://umclidet.com/pdf/Magic.Tricks.for.the.Beginning.Magician.pdf",
+  "size": 123,
+  "fileName": "Magic.Tricks.for.the.Beginning.Magician.pdf",
+  "storageLocation": "local"
+  }' $URL$RESOURCE
+
+  RESOURCE="/4/files"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{
+  "title": "Learning to drive a manual car",
+  "filePath": "http://www.driverighttraining.com.au/docs/How-to-Drive-a-Manual-Car.pdf",
+  "size": 123,
+  "fileName": "How-to-Drive-a-Manual-Car.pdf",
+  "storageLocation": "local"
+  }' $URL$RESOURCE
+
+fi
+
+# Section 5 Add Submission
+if [ $ADD_SUBMISSION = "true" ]; then
+
+  RESOURCE="/3/submissions"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "submitterId": 30 }' $URL$RESOURCE
+
+  RESOURCE="/4/submissions"
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "submitterId": 30 }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"  -d '{ "submitterId": 50 }' $URL$RESOURCE
+
+
+fi
+
+# Section 6 Add a submission File
+if [ $ADD_SUBMISSION_FILES = "true" ]; then
+  RESOURCE="/3/submissions/1/files"
+  echo '\n Add a summision file for chal 3 submission 1'
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "title": "user 30 sbumission - Magic My way",
+    "filePath": "/uploads/challengeId/",
+    "size": 123,
+    "fileName": "magiclLesson1.zip",
+    "storageLocation": "local"
+  }'  $URL$RESOURCE
+
+    RESOURCE="/4/submissions/2/files"
+    echo '\n Add a summision file for chal 4 submission 2'
+    curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "title": "user 50 sbumission -the manual transmission a beginners guide",
+    "filePath": "/uploads/challengeId/",
+    "size": 123,
+    "fileName": "src.zip",
+    "storageLocation": "local"
+  }'  $URL$RESOURCE
+
+    RESOURCE="/4/submissions/3/files"
+    echo '\n Add a summision file for chal 4 submission 3'
+    curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "title": "How to drive a stick - Magic My way user50",
+    "filePath": "/uploads/challengeId/",
+    "size": 123,
+    "fileName": "learnStick.zip",
+    "storageLocation": "local"
+  }'  $URL$RESOURCE
+fi
+
+# Section 7 add requirments
+if [ $ADD_REQUIREMENTS = "true" ]; then
+
+  RESOURCE="/1/requirements"
+  echo '\n Add a requirments for challenge 1 challenge to rule them all \n'
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 1,
+    "requirementText": "An immortal Scottish swordsman must confront the last of his immortal opponent, a murderously brutal barbarian who lusts for the fabled Prize.   Define the prize "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 1,
+    "requirementText": "Describe the most ultimate soundtrack by British Rockstars Queen"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 1,
+    "requirementText": "Define the actual meaning of the word Kurgen "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 1,
+    "requirementText": "The Kurgens voice can be found in what popular modern cartoon, and the spokesmen for what Home imporment store "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 1,
+    "requirementText": "Name the type of sword found by the NY police department by a severed head "
+  }' $URL$RESOURCE
+
+
+  RESOURCE="/2/requirements"
+  echo '\n Add a requirments for challenge 2 2 tango \n'
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 2,
+    "requirementText": "Describe the history of the Tango "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 2,
+    "requirementText": "Who played Tango and Cash"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 2,
+    "requirementText": "Who said The secret of tango is in the moment of improvisation that happens between step and step. It is to make the impossible thing possible: to dance silence. "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 2,
+    "requirementText": "What movie am I thinking of:  Marlon Brando and Maria Schneider"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 2,
+    "requirementText": "What two cultures influenced this provatice dance which is the latin word for touch"
+  }' $URL$RESOURCE
+
+  RESOURCE="/3/requirements"
+  echo '\n Add a requirments for challenge 3 is a magic number \n'
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "What 1973 educational cartoon  wrote this song and who is the original singer"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "What 90 grunge band made this song rock"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "A man and a woman had a little baby.   Complete this verse "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "What number am I thinking of"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "Translate 3 into as many languages as you can."
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 3,
+    "requirementText": "Name every object portraied in this video"
+  }' $URL$RESOURCE
+
+  RESOURCE="/4/requirements"
+  echo '\n Add a requirments for challenge 4 on the floor \n'
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "Describe how to start a manual car without a battery"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "Describe the different configurations of a manual tranmssion"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "Guess how many cars I have owned with a stick shift "
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "What is the name of the pedel that cars with automatic transmissions dont have,   What is its purpose?"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "Describe the purpose of the Tachometer"
+  }' $URL$RESOURCE
+  curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache"   -d '{
+    "challengeId": 4,
+    "requirementText": "Describe how a manual transmission can slow a car down without the breaks."
+  }' $URL$RESOURCE
+
+
+
+
 
 fi
