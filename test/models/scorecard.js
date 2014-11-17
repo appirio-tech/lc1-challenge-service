b353a22a1b710fbf17d49e68fb17e308e247f07c
Mulili Nzuki <mulili.nzuki@gmail.com>
2014-11-18 00:07:38 +0300

    Added the _handle field to the scorecards, submissions and participants tables

--- test/models/scorecard.js
+++ test/models/scorecard.js
@@ -44,7 +44,7 @@ describe('<Unit Test>', function() {
         prize: 1500,        
         challengeId: 111,
         reviewerId: 222,
-        reviewerHandle: 'test_reviewer',
+        reviewer_handle: 'test_reviewer',
         submissionId: 333,
         createdBy: 1,
         updatedBy: 1
@@ -63,7 +63,7 @@ describe('<Unit Test>', function() {
           savedEntity.scoreSum.should.equal(data.scoreSum);
           savedEntity.status.should.equal(data.status);
           savedEntity.submissionId.should.equal(data.submissionId);
-          savedEntity.reviewerHandle.should.equal(data.reviewerHandle);
+          savedEntity.reviewer_handle.should.equal(data.reviewer_handle);
           savedEntity.createdBy.should.equal(data.createdBy);
           savedEntity.updatedBy.should.equal(data.updatedBy);
           done();
