e26fc57b86c7ce6f756a45b105958858c72fbcad
Neil Hastings <neil.hastings@gmail.com>
2014-11-20 18:53:08 -0800

    Add morgan

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ var routeHelper = require('./lib/routeHelper');
 var partialResponseHelper = require('./lib/partialResponseHelper');
 var bodyParser = require('body-parser');
 var request = require('request');
+var morgan = require('morgan')
 
 var app = express();
 
 app.use(bodyParser.json());
+
+app.use(morgan(':req[Authorization]'));
 
 // Add tc user
 // @TODO Move this into it's own module
