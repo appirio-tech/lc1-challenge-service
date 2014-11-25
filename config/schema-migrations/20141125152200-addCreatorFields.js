c594c48c210d8d52cb19e0af93162bcd720b47b8
Cain Wong <countcainht@gmail.com>
2014-11-26 00:16:12 +0800

    update migration script

--- config/schema-migrations/20141125152200-addCreatorFields.js
+++ config/schema-migrations/20141125152200-addCreatorFields.js
@@ -4,14 +4,14 @@ var type = dbm.dataType;
 
 exports.up = function(db, callback) {
   async.series([
-    db.runSql.bind(db, 'ALTER TABLE ONLY challenges ADD COLUMN creatorHandle character varying(128);'),
-    db.runSql.bind(db, 'ALTER TABLE ONLY challenges ADD COLUMN creatorId bigint;')
+    db.runSql.bind(db, 'ALTER TABLE ONLY challenges ADD COLUMN "creatorHandle" character varying(128);'),
+    db.runSql.bind(db, 'ALTER TABLE ONLY challenges ADD COLUMN "creatorId" bigint;')
   ], callback);
 };
 
 exports.down = function(db, callback) {
   async.series([
-    db.runSql.bind(db, 'ALTER TABLE ONLY challenges DROP COLUMN IF EXISTS creatorHandle;'),
-    db.runSql.bind(db, 'ALTER TABLE ONLY challenges DROP COLUMN IF EXISTS creatorId;')
+    db.runSql.bind(db, 'ALTER TABLE ONLY challenges DROP COLUMN IF EXISTS "creatorHandle";'),
+    db.runSql.bind(db, 'ALTER TABLE ONLY challenges DROP COLUMN IF EXISTS "creatorId";')
   ], callback);
 };
