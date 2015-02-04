755c3cafce7a272d3765f3fd213d91c11cf7b852
Neil Hastings <neil.hastings@gmail.com>
2015-02-04 12:25:36 -0500

    FIx permission checks on storage controller

--- api/controllers/storage.js
+++ api/controllers/storage.js
@@ -9,7 +9,6 @@
 var datasource = require('./../../datasource').getDataSource();
 var Challenge = datasource.Challenge;
 var File = datasource.File;
-var routeHelper = require('serenity-route-helper');
 var async = require('async');
 var auth = require('serenity-auth');
 var config = require('config');
@@ -41,117 +40,188 @@ var findById = function(Model, filters, callback) {
  */
 var getChallengeFileURL = function(method, req, res, next) {
   // check authorization
-  var challengeId = req.swagger.params.challengeId.value,
-    fileId = req.swagger.params.fileId.value,
-    user = auth.getSigninUser(req);
+  var challengeId = req.swagger.params.challengeId.value;
+  var fileId = req.swagger.params.fileId.value;
+  var user = auth.getSigninUser(req);
+  var currentUserIsAdmin = auth.currentUserPass(req);
 
-  async.waterfall([
-    function(cb) {
-      findById(Challenge, {where: {id:challengeId}}, cb);
-    },
-    function(challenge, cb) {
-      if(!challenge) {
-        cb(new errors.NotFoundError("challenge"));
-      }
-
-      if (!auth.currentUserIsSafe(req)) {
-        challenge.getParticipants({where: {userId: user.id}}).success(function(participants) {
-          cb(null, participants);
-        }).error(function(err) {
-          cb(err);
-        });
-      } else {
-        cb(null, null);
-      }
-
-    },
-    function(participants, cb) {
-      if (!auth.currentUserIsSafe(req)) {
-        // participant will be an array, should not be empty array
-        if (!participants || participants.length === 0) {
-          cb(new errors.NotPermittedError('User is not a Participant'));
-        }
-      }
-      findById(File, {where: {id:fileId, challengeId: challengeId}}, cb);
-    },
-    function(file, cb) {
-      if(!file) {
-        cb(new errors.NotFoundError("file"));
-      }
-      storageLib[method](file, cb);
-    }
-  ], function(err, result) {
-    if (err) {
-      return next(err);  // go to error handler
-    } else {
-      req.data = {
-        success: true,
-        status: 200,
-        metadata: {
-          totalCount: 1
+  if (user) {
+    if (currentUserIsAdmin) {
+      async.waterfall([
+        function(cb) {
+          findById(Challenge, {where: {id:challengeId}}, cb);
         },
-        content: {url: result}
-      };
+        function(challenge, cb) {
+          if (!challenge) {
+            return cb(new errors.NotFoundError('challenge'));
+          }
+
+          findById(File, {where: {id:fileId, challengeId: challengeId}}, cb);
+        },
+        function(file, cb) {
+          if(!file) {
+            return cb(new errors.NotFoundError('file'));
+          }
+          storageLib[method](file, cb);
+        }
+      ], function(err, result) {
+          if (err) {
+            return next(err);  // go to error handler
+          } else {
+            req.data = {
+              success: true,
+              status: 200,
+              metadata: {
+                totalCount: 1
+              },
+              content: {url: result}
+            };
+          }
+          next();
+        }
+      );
+    } else {
+      async.waterfall([
+        function(cb) {
+          findById(Challenge, {where: {id:challengeId}}, cb);
+        },
+        function(challenge, cb) {
+          if(!challenge) {
+            return cb(new errors.NotFoundError('challenge'));
+          }
+
+          challenge.getParticipants({where: {userId: user.id}}).success(function(participants) {
+            cb(null, participants);
+          }).error(function(err) {
+            cb(err);
+          });
+
+        },
+        function(participants, cb) {
+          // participant will be an array, should not be empty array
+          if (!currentUserIsAdmin && (!participants || participants.length === 0)) {
+            return cb(new errors.NotPermittedError('User is not a Participant'));
+          }
+          findById(File, {where: {id:fileId, challengeId: challengeId}}, cb);
+        },
+        function(file, cb) {
+          if(!file) {
+            return cb(new errors.NotFoundError('file'));
+          }
+          storageLib[method](file, cb);
+        }
+      ], function(err, result) {
+        if (err) {
+          return next(err);  // go to error handler
+        } else {
+          req.data = {
+            success: true,
+            status: 200,
+            metadata: {
+              totalCount: 1
+            },
+            content: {url: result}
+          };
+        }
+
+        next();
+      });
     }
 
-    next();
-  });
+  } else {
+    next(new errors.AuthenticationRequiredError('Authentication Required'));
+  }
 };
 
 var getSubmissionFileURL = function(method, req, res, next) {
-  var challengeId = req.swagger.params.challengeId.value,
-    submissionId = req.swagger.params.submissionId.value,
-    user = auth.getSigninUser(req),
-    fileId = req.swagger.params.fileId.value;
+  var challengeId = req.swagger.params.challengeId.value;
+  var submissionId = req.swagger.params.submissionId.value;
+  var user = auth.getSigninUser(req);
+  var fileId = req.swagger.params.fileId.value;
+  var currentUserIsAdmin = auth.currentUserPass(req);
 
-  async.waterfall([
-    function(cb) {
-      findById(Challenge, {where: {id:challengeId}}, cb);
-    },
-    function(challenge, cb) {
-      if(!challenge) {
-        cb(new errors.NotFoundError("challenge"));
-      }
+  if (user) {
+    if (currentUserIsAdmin) {
+      async.waterfall([
+          function(cb) {
+            findById(Challenge, {where: {id:challengeId}}, cb);
+          },
+          function(challenge, cb) {
+            if (!challenge) {
+              return cb(new errors.NotFoundError('challenge'));
+            }
 
-      if (!auth.currentUserIsSafe(req)) {
-        challenge.getSubmissions({where: {submitterId: user.id}}).success(function (submissions) {
-          cb(null, submissions);
-        }).error(function (err) {
-          cb(err);
-        });
-      } else {
-        cb(null, null);
-      }
-    },
-    function(submissions, cb) {
-      if (!auth.currentUserIsSafe(req)) {
-        if (!submissions || submissions.length === 0) {
-          cb(new errors.NotPermittedError('User is not authorized'));
+            findById(File, {where: {id:fileId, submissionId: submissionId}}, cb);
+          },
+          function (file, cb) {
+            if (!file) {
+              return cb(new errors.NotFoundError('file'));
+            }
+            storageLib[method](file, cb);
+          }
+        ], function (err, result) {
+          if (err) {
+            return next(err);  // go to error handler
+          } else {
+            req.data = {
+              success: true,
+              status: 200,
+              metadata: {
+                totalCount: 1
+              },
+              content: {url: result}
+            };
+          }
+          next();
         }
-      }
-      findById(File, {where: {id:fileId, submissionId: submissionId}}, cb);
-    },
-    function(file, cb) {
-      if(!file) {
-        cb(new errors.NotFoundError("file"));
-      }
-      storageLib[method](file, cb);
-    }
-  ], function(err, result) {
-    if(err) {
-      return next(err);  // go to error handler
+      );
     } else {
-      req.data = {
-        success: true,
-        status: 200,
-        metadata: {
-          totalCount: 1
+      async.waterfall([
+        function(cb) {
+          findById(Challenge, {where: {id:challengeId}}, cb);
         },
-        content: {url: result}
-      };
+        function(challenge, cb) {
+          if(!challenge) {
+            return cb(new errors.NotFoundError('challenge'));
+          }
+
+          challenge.getSubmissions({where: {submitterId: user.id}}).success(function (submissions) {
+            cb(null, submissions);
+          }).error(function (err) {
+            cb(err);
+          });
+        },
+        function(submissions, cb) {
+          if (!currentUserIsAdmin && (!submissions || submissions.length === 0)) {
+            return cb(new errors.NotPermittedError('User is not authorized'));
+          }
+          findById(File, {where: {id:fileId, submissionId: submissionId}}, cb);
+        },
+        function(file, cb) {
+          if(!file) {
+            return cb(new errors.NotFoundError('file'));
+          }
+          storageLib[method](file, cb);
+        }
+      ], function(err, result) {
+        if(err) {
+          return next(err);  // go to error handler
+        } else {
+          req.data = {
+            success: true,
+            status: 200,
+            metadata: {
+              totalCount: 1
+            },
+            content: {url: result}
+          };
+        }
+        next();
+      });
     }
-    next();
-  });
+  } else {
+    next(new errors.AuthenticationRequiredError('Authentication Required'));
+  }
 };
 
 /**
